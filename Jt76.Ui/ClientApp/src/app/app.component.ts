22dc5cc2eac6b8099c285eee003141eec1d67b6a
JeremyTurner1976 <jeremyturner1976@eagles.ewu.edu>
2018-01-28 02:19:43 -0700

    Finalized layout with three custom themes

--- Jt76.Ui/ClientApp/src/app/app.component.ts
+++ Jt76.Ui/ClientApp/src/app/app.component.ts
@@ -7,17 +7,30 @@ import { Component, OnInit } from "@angular/core";
 })
 export class AppComponent implements OnInit {
   title = "Jt76.Ui";
+  theme: string;
   themeClass: string;
   isAuthenticated = false;
+  year = new Date().getFullYear();
 
   ngOnInit(): void {
-    this.themeClass = "dark-theme";
+    this.setTheme("dark");
   }
 
   setTheme(theme) {
-    console.log(theme);
-    console.log(theme);
+    this.theme = theme;
     this.themeClass = theme + "-theme";
+  }
+
+  colorTheme() {
+    var standardClasses = "content mat-card";
+    switch (this.theme) {
+      case "dark":
+        return `dark-colors ${standardClasses}`;
+      case "default":
+        return `default-colors ${standardClasses}`;
+      case "light":
+        return `light-colors ${standardClasses}`;
+    }
   }
 
   onLoginClick() {
